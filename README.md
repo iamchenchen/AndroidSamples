0f69815e2f97e1eaf9140cd7e176795df8678119
Chenchen Zheng <chenchen.zheng@fireeye.com>
2014-02-05 14:42:39 -0800

    update README and added signIn project

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 AndroidSamples
 ==============
 
+Description
+___________
+
 My Android Sample Projects, these libraries can be used for future app implementation.
+
+Project Detail:
+===============
+
+*	signIn
+_________
+
+This is a sample project for simple sign in screen.
+
---
+++ signIn/src/com/iamchenchen/signin/ActivationActivity.java
@@ -0,0 +1,24 @@
+package com.iamchenchen.signin;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ActivationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
